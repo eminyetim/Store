334daa37cc14b506d838a2fec261b9ed01df1ca0
Emin YTM <eminyetim25@gmail.com>
2024-11-26 20:07:18 +0300

    gitignore migrations

--- Repositories/obj/Debug/net6.0/Repositories.AssemblyInfo.cs
+++ Repositories/obj/Debug/net6.0/Repositories.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52a10a6dd68e210c32fec096de1a119a5188ba09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b02f27cb49d2f3d0011b960487208162ebbd7902")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
